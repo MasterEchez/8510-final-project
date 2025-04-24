a3846e14ee8e98c67b3b22d48eee27250409b609
MasterEchez <eechezona9@gmail.com>
2025-04-24 05:01:46 -0400

    formatting

--- scripts/battle-helper.js
+++ scripts/battle-helper.js
@@ -1,25 +1,25 @@
-import {battleStates, gestures, gestureToChoice} from './constants.js';
+import { battleStates, gestures, gestureToChoice } from "./constants.js";
 
 export const simplifySideUpdate = (serverOutput) => {
-    // https://github.com/smogon/pokemon-showdown/blob/master/sim/SIM-PROTOCOL.md#choice-requests
-    const [_, type, preparsed] = serverOutput.split('|');
-    const obj = JSON.parse(preparsed);
-    let simpleObj = {};
-    console.log(serverOutput);
+  // https://github.com/smogon/pokemon-showdown/blob/master/sim/SIM-PROTOCOL.md#choice-requests
+  const [_, type, preparsed] = serverOutput.split("|");
+  const obj = JSON.parse(preparsed);
+  let simpleObj = {};
+  console.log(serverOutput);
 
-    switch (type) {
-        case 'request':
-            simpleObj = {
-                active: obj.active[0].moves,
-                party: obj.side.pokemon
-            };
-            break;
-        case 'error':
-            break;
-        default:
-            console.log(`battle helper weird type: ${type}`);
-            break;
-    }
-    
-    return [type, simpleObj];
-}
+  switch (type) {
+    case "request":
+      simpleObj = {
+        active: obj.active[0].moves,
+        party: obj.side.pokemon,
+      };
+      break;
+    case "error":
+      break;
+    default:
+      console.log(`battle helper weird type: ${type}`);
+      break;
+  }
+
+  return [type, simpleObj];
+};
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,29 +1,29 @@
 export const battleStates = {
-    CONFIRM_USERS: 'confirm_users',
-    BATTLE_WAITING: 'battle_waiting', // waiting for player moves
-    BATTLE_SHOW: 'battle_show', // show results of turn
-    BATTLE_OVER: 'battle_over',
-}
+  CONFIRM_USERS: "confirm_users",
+  BATTLE_WAITING: "battle_waiting", // waiting for player moves
+  BATTLE_SHOW: "battle_show", // show results of turn
+  BATTLE_OVER: "battle_over",
+};
 
 export const gestures = {
-    THUMBS_UP: 'thumbs_up',
-    THUMBS_DOWN: 'thumbs_down',
-    VICTORY: 'victory',
-    POINTING_UP: 'point_up',
-    CLOSED_FIST: 'closed_fist',
-    OPEN_PALM: 'open_palm',
-    LOVE: 'love'
-}
+  THUMBS_UP: "thumbs_up",
+  THUMBS_DOWN: "thumbs_down",
+  VICTORY: "victory",
+  POINTING_UP: "point_up",
+  CLOSED_FIST: "closed_fist",
+  OPEN_PALM: "open_palm",
+  LOVE: "love",
+};
 
 export const gestureToChoice = (gesture) => {
-    switch (gesture) {
-        case gestures.THUMBS_UP:
-            return '1';
-        case gestures.THUMBS_DOWN:
-            return '2';
-        case gestures.VICTORY:
-            return '3';
-        case gestures.POINTING_UP:
-            return '4';
-    }
-}
+  switch (gesture) {
+    case gestures.THUMBS_UP:
+      return "1";
+    case gestures.THUMBS_DOWN:
+      return "2";
+    case gestures.VICTORY:
+      return "3";
+    case gestures.POINTING_UP:
+      return "4";
+  }
+};
