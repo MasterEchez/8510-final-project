dd1f4bf3a638dbc7d2cc21b600d6efe37a8327a7
MasterEchez <eechezona9@gmail.com>
2025-04-23 23:52:10 -0400

    add link to example request obj

--- scripts/battle-helper.js
+++ scripts/battle-helper.js
@@ -1,6 +1,7 @@
 import {battleStates, gestures, gestureToChoice} from './constants.js';
 
 export const simplifyServerOutput = (serverOutput) => {
+    // https://github.com/smogon/pokemon-showdown/blob/master/sim/SIM-PROTOCOL.md#choice-requests
     const obj = JSON.parse(serverOutput.split('|')[2]);
 
     const simpleObj = {
