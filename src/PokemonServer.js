e42d14d7a15480409c05b3391422f7bc9e483df4
MasterEchez <eechezona9@gmail.com>
2025-05-05 18:42:01 -0400

    backends for server

--- src/PokemonServer.js
+++ src/PokemonServer.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const Sim = require("pokemon-showdown");
+const { battleStates, gestures, gestureToChoice } = require("../scripts/constants");
+const { simplifySideUpdate } = require("../scripts/battle-helper");
+
+let simState = battleStates.CONFIRM_USERS;
+
+// list of events to animate
+let toAnimate = [];
+
+
+//
+//
+//
+// SERVER STARTED
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+
+app.post('start', (req, res) => {
+    // start rand bat, return data
+});
+
+app.post('gestures', (req, res) => {
+    // record gestures, update state machine, return relevant update
 });
 
 app.listen(port, () => {
