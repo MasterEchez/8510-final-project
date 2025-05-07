49823598fe5958b9d9a05e7f33685cbff1cc0864
MasterEchez <eechezona9@gmail.com>
2025-05-07 19:37:55 -0400

    backend logic

--- src/PokemonServer.js
+++ src/PokemonServer.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const Sim = require("pokemon-showdown");
+const {
+  Sim,
+  SwitchRequest,
+  MoveRequest,
+  WaitRequest,
+} = require("pokemon-showdown");
 const {
   battleStates,
   gestures,
@@ -7,23 +12,72 @@ const {
 } = require("../scripts/constants");
 const { simplifySideUpdate } = require("../scripts/battle-helper");
 
-simState = battleStates.BATTLE_SHOW;
 const stream = new Sim.BattleStream();
-const streams = new Sim.getPlayerStreams(stream);
-const p1Stream = streams.p1;
-const p2Stream = streams.p2;
-const omniStream = streams.omniscient;
+
+let [p1_updated, p2_updated, omni_updated] = [false, false, false];
+
+let p1 = "";
+let p2 = "";
+let omni = "";
+
+const p1_updated_promise = async () => {
+  while (!p1_updated) {
+    await new Promise((resolve) => setTimeout(resolve, 200));
+  }
+  return;
+};
+
+const p2_updated_promise = async () => {
+  while (!p2_updated) {
+    await new Promise((resolve) => setTimeout(resolve, 200));
+  }
+  return;
+};
+
+const omni_updated_promise = async () => {
+  while (!omni_updated) {
+    await new Promise((resolve) => setTimeout(resolve, 200));
+  }
+  return;
+};
 
 let getState = async () => {
+  console.log("________");
   console.log("here");
-  let p1 = (await p1Stream.next()).value;
-  let p2 = (await p2Stream.next()).value;
-  let omni = (await omniStream.next()).value;
-  console.log(p1);
-  console.log(p2);
-  console.log(omni);
+  await Promise.all([
+    p1_updated_promise(),
+    p2_updated_promise(),
+    omni_updated_promise(),
+  ]);
   return { p1, p2, omni };
 };
+
+(async () => {
+  for await (const output of stream) {
+    const msgParts = output.split("\n");
+    switch (msgParts[0]) {
+      case "sideupdate":
+        if (msgParts[1] === "p1") {
+          p1 = msgParts[2];
+          p1_updated = true;
+          // console.log(p1);
+        } else {
+          p2 = msgParts[2];
+          p2_updated = true;
+          // console.log(p2);
+        }
+        break;
+      case "update":
+        omni = msgParts;
+        omni_updated = true;
+        // console.log(msgParts);
+        break;
+      default:
+        console.log(output);
+        break;
+    }
+  }
+})();
 
 //
 //
@@ -40,24 +94,27 @@ app.get("/", (req, res) => {
 
 app.post("/start", async (req, res) => {
   // start rand bat, return data
+  [p1_updated, p2_updated, omni_updated] = [false, false, false];
   await stream.write(
     `>start {"formatid":"gen9randombattle"}\n` +
       `>player p1 {"name":"Player1"}\n` +
       `>player p2 {"name":"Player2"}`
   );
   const msg = await getState();
-  console.log(msg);
+  // console.log(msg);
   res.send(msg);
 });
 
 app.post("/moves", async (req, res) => {
   // writes moves to stream and returns output
   let body = req.body;
-  console.log(body);
-  await stream.write(
-    `>p1 ${body.p1}\n` + `>p2 ${body.p2}`
-  );
+  // console.log(body);
+  [p1_updated, p2_updated, omni_updated] = [false, false, false];
+  const p1_part = body.p1 ? `>p1 ${body.p1}\n` : "";
+  const p2_part = body.p2 ? `>p2 ${body.p2}` : "";
+  await stream.write(p1_part + p2_part);
   const msg = await getState();
+  // console.log(msg);
   res.send(msg);
 });
 
