cf9af16b5a189bea9df07c3811aaa0f45d4091a4
MasterEchez <eechezona9@gmail.com>
2025-05-08 00:07:30 -0400

    settle at animate state

--- src/states/BattleShowState.js
+++ src/states/BattleShowState.js
@@ -9,16 +9,28 @@ class BattleShowState {
     return battleStates.BATTLE_SHOW;
   }
 
+  updatePlayersReady() {
+
+  }
+
   display() {
     const hud = document.getElementById("hud");
     const players = document.getElementById("players");
 
     const header = createElementWithText(
       "h1",
-      "Turn"
+      "Make gesture for move/switch"
     );
 
     hud.insertBefore(header, players);
+
+    // get gesture
+    const p1_gesture = createElementWithText("span", "p1_gest");
+    player1.appendChild(p1_gesture);
+
+    // get gesture
+    const p2_gesture = createElementWithText("span", "p2_gest");
+    player2.appendChild(p2_gesture);
   }
 
   isStateDone() {}
